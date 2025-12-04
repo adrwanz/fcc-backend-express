31d12224613b1153db430179fbd96da1c8ea9b48
adrwanz <adrwanz01@gmail.com>
2025-12-04 20:01:37 -0300

    Ejercicio 6.2

--- myApp.js
+++ myApp.js
@@ -1,38 +1,18 @@
+
 require('dotenv').config();
 let express = require('express');
 let app = express();
-// Ejercicio 5: servir JSON en /json
-/*app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello json"
-  });
-});*/
-/*Ejercicio 6*/
-app.get("/json", (req, res) => {
-  let message = "Hello json";
-  if (process.env.MESSAGE_STYLE === "uppercase") {
+
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+app.get('/json', (req, res) => {
+  let message = 'Hello json';
+  if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
   res.json({ message: message });
 });
-
-// Ejercicio: 4 servir activos estáticos desde la carpeta /public
-app.use("/public", express.static(__dirname + "/public"));
-// Ejercicio: 3 servir el archivo index.html en la ruta raíz
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
-});
-// Ejercicio: 2 servir la cadena en la ruta raíz
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
-
-// Ejercicio: 1 "Hello World"
-/*console.log("Hello World");
-app.get("/json", (req, res) => {
-  res.json({
-    message: "Hello World"
-  });
-});*/
 
 module.exports = app;
