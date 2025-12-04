1afd9778acd682f2221ec5de1a6af1942456678e
adrwanz <adrwanz01@gmail.com>
2025-12-04 20:20:06 -0300

    Ejercicio 6.5

--- myApp.js
+++ myApp.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/json', (req, res) => {
-  let message = 'HELLO JSON';
+  let message = 'Hello json';
   if (process.env.MESSAGE_STYLE === 'uppercase') {
     message = message.toUpperCase();
   }
