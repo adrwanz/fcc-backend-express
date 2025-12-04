ddff7852578be82fbed7a3b77a39ef620fe87339
adrwanz <adrwanz01@gmail.com>
2025-12-04 19:39:42 -0300

    Ejercicio 3

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 let express = require('express');
 let app = express();
+// Ejercicio: 3 servir el archivo index.html en la ruta raíz
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 // Ejercicio: 2 servir la cadena en la ruta raíz
 app.get("/", (req, res) => {
   res.send("Hello Express");
