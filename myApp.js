bbc7d06748d794bbb02f62d546d4d2069101abd9
adrwanz <adrwanz01@gmail.com>
2025-12-04 19:34:55 -0300

    Ejercicio 2

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 let express = require('express');
 let app = express();
+// Ejercicio: 2 servir la cadena en la ruta raíz
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+// Ejercicio: 1 "Hello World"
 console.log("Hello World");
-// Ejercicio: crear la ruta /json
 app.get("/json", (req, res) => {
   res.json({
     message: "Hello World"
